4bcbb905e7d56f5ad2f8e98da2a08566662524c9
MeiNagano <3291922284@qq.com>
2026-08-20 20:19:33 +0800

    chore: 版本号 1.0.7 / versionCode 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "sb.linux.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
     }
 
     // 4.1 CI 构建 armv8a / armv7a / 通用版三个 APK
